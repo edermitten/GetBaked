e90c63cf3805e1dc701c92c5983a89c5fa707621
Eder <edermitten@gmail.com>
2023-12-11 14:39:20 -0500

    rebuild shop page to be dynamically

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GetBaked.Data;
 using GetBaked.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
 
 namespace GetBaked.Controllers
 {
+    [Authorize(Roles = "Administrator")]
     public class CategoriesController : Controller
     {
         private readonly ApplicationDbContext _context;
--- Controllers/ShopController.cs
+++ Controllers/ShopController.cs
@@ -1,28 +1,53 @@
-﻿using GetBaked.Models;
+﻿using GetBaked.Data;
+using GetBaked.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GetBaked.Controllers
 {
     public class ShopController : Controller
     {
-        public IActionResult Index()
+        //ADD DB CONECCION
+        private readonly ApplicationDbContext _context;
+
+        public ShopController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
         }
 
-        public IActionResult ByCategory(String name)
+        public IActionResult Index()
+        {
+            //use db conection to fect categories
+            var categories = _context.Categories.OrderBy(c=>c.Name).ToList();
+
+            //pass categories to the view
+            return View(categories);
+        }
+
+        
+        public IActionResult ByCategory(int id)
         {
             //store category name
-            ViewData["Category"] = name;
+            var category = _context.Categories.Find(id);
 
-            //list of products
-            var products = new List<Product>();
+            //return to shop if category not found
+            if(category == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            ViewData["Category"] = category.Name;
+
+            //list of products 
+            var products = _context.Products.Where(p=>p.CategoryId == id)
+                .OrderBy(p=>p.Name)
+                .ToList();
 
             //loop for products
-            for (int i = 1;i <=5; i++)
+            /*for (int i = 1;i <=5; i++)
             {
                 products.Add(new Product { ProductId = i, Name = "Product" + i.ToString(), Price = 8 });
             }
+            */
 
             //send the products
             //return View();
